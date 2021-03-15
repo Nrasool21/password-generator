2dfc4081602fc5798389914686e450140e79c610
Nazia R <nazia.huma@gmail.com>
2021-03-15 16:23:46 +0000

    declared generatePassword with if else statement

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  /*store a variable */
+  const passwordLength = prompt(
+    "Specify the length of your password",
+    "enter number here"
+  );
 
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    /*link it with var passwordchar*/
+    console.log(passwordChar()); 
+  } else {
+    alert("invalid submission");
+
+  }
+}
+
+/*declare another variable to verify password character*/
+const passwordChar = new Array(4);
+
+for (i = 0; i < 4; i++) {
+  if (i === 4) {break}
+  passwordChar[i] = confirm("Your password must include an Uppercase ","");
+  passwordChar[i] = confirm("Your password must include a Lowercase ", "");
+  passwordChar[i] = confirm("Your password must include a Number ", "");
+  passwordChar[i] = confirm("Your password must include a Special Character ", "");
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
